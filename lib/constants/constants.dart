381a7f031efec146a7fea9821007ba1c0900c4c5
akshatsri11 <akshat13579@gmail.com>
2022-12-15 18:12:13 +0530

    third commit

---
+++ lib/constants/constants.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+const myGradient = [
+  LinearGradient(colors: [Colors.black, Colors.red], begin: Alignment.topRight),
+  LinearGradient(
+      colors: [Colors.black, Colors.purple], begin: Alignment.topRight),
+  LinearGradient(
+      colors: [Colors.black, Colors.amber], begin: Alignment.topRight),
+  LinearGradient(colors: [Colors.black, Colors.red], begin: Alignment.topRight),
+  LinearGradient(colors: [Colors.black, Colors.red], begin: Alignment.topRight),
+  LinearGradient(
+      colors: [Colors.black, Colors.purple], begin: Alignment.topRight),
+  LinearGradient(
+      colors: [Colors.black, Colors.amber], begin: Alignment.topRight),
+  LinearGradient(colors: [Colors.black, Colors.red], begin: Alignment.topRight),
+];
+
+final pics = [
+  "lib/images/BB.png",
+  "lib/images/CM.png",
+  "lib/images/PDP.jpg",
+  "lib/images/BB2.jpg",
+  "lib/images/BB.png",
+  "lib/images/CM.png",
+  "lib/images/PDP.jpg",
+  "lib/images/BB2.jpg",
+];
+
+final names = [
+  "BB ki Vines",
+  "Carrryminati",
+  "PewDiePie",
+  "BB ki Vines",
+  "BB ki Vines",
+  "Carrryminati",
+  "PewDiePie",
+  "BB ki Vines",
+];
+
+final des = [
+  "Youtuber",
+  "Youtuber",
+  "Musican",
+  "Actor",
+  "Youtuber",
+  "Youtuber",
+  "Musican",
+  "Actor"
+];
+
+const voilet = Color.fromARGB(255, 132, 0, 255);
---
+++ lib/screens/BonusScreen.dart
@@ -0,0 +1,168 @@
+// ignore_for_file: prefer_typing_uninitialized_variables, prefer_const_constructors, file_names
+
+import 'package:flutter/material.dart';
+import 'package:tiobu/constants/constants.dart';
+
+class RecordScreen extends StatefulWidget {
+  const RecordScreen({super.key, this.name, this.amount, this.message});
+  final name;
+  final amount;
+  final message;
+  @override
+  State<RecordScreen> createState() => _RecordScreenState();
+}
+
+class _RecordScreenState extends State<RecordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: voilet,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 5),
+                  borderRadius: BorderRadius.circular(20)),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  'Donars',
+                  style: TextStyle(color: Colors.white, fontSize: 70),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Table(
+                defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                border: TableBorder.all(color: Colors.white),
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  TableRow(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            'Creator id',
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                      ),
+                      Center(
+                          child: Text('Curency',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 15))),
+                      Center(
+                          child: Text('Amount',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 15))),
+                      Center(
+                          child: Text('Name',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 15))),
+                      Center(
+                          child: Text('Message',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 15))),
+                    ],
+                  ),
+                  TableRow(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Center(
+                          child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Text('BB ki Vines',
+                            style: TextStyle(color: Colors.white)),
+                      )),
+                      Center(
+                        child: Icon(
+                          Icons.attach_money,
+                          size: 15,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Center(
+                          child:
+                              Text('', style: TextStyle(color: Colors.white))),
+                      Center(
+                          child:
+                              Text('', style: TextStyle(color: Colors.white))),
+                      Center(
+                          child:
+                              Text('', style: TextStyle(color: Colors.white))),
+                    ],
+                  ),
+                  TableRow(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Center(
+                          child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Text('Carryminati',
+                            style: TextStyle(color: Colors.white)),
+                      )),
+                      Center(
+                        child: Icon(
+                          Icons.attach_money,
+                          size: 15,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Center(
+                          child:
+                              Text('', style: TextStyle(color: Colors.white))),
+                      Center(
+                          child:
+                              Text('', style: TextStyle(color: Colors.white))),
+                      Center(
+                          child:
+                              Text('', style: TextStyle(color: Colors.white))),
+                    ],
+                  ),
+                  TableRow(
+                    // ignore: prefer_const_literals_to_create_immutables
+                    children: [
+                      Center(
+                          child: Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Text('PewDiePie',
+                            style: TextStyle(color: Colors.white)),
+                      )),
+                      Center(
+                        child: Icon(
+                          Icons.attach_money,
+                          size: 15,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Center(
+                          child:
+                              Text('', style: TextStyle(color: Colors.white))),
+                      Center(
+                          child:
+                              Text('', style: TextStyle(color: Colors.white))),
+                      Center(
+                          child:
+                              Text('', style: TextStyle(color: Colors.white))),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
